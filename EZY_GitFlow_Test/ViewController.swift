49db80d880cd36f87df958adf240be3333c574ee
Y00ujin <yujingim43@gmail.com>
2021-05-22 12:35:39 +0900

    [INIT] Initialize

---
+++ EZY_GitFlow_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EZY_GitFlow_Test
+//
+//  Created by 김유진 on 2021/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // EZY GitFlow Test
+    }
+
+
+}
+
